9afad2e5a9ea86610b4f1cef33656867006e3a85
derrickip34 <derrickip34@gmail.com>
2020-04-24 13:38:10 +0300

    add articles route

--- app/views.py
+++ app/views.py
@@ -1,7 +1,16 @@
 from flask import render_template
 from app import app
+from .request import get_sources,get_articles
 
 @app.route('/')
 def index():
 
-    return render_template('index.html')
+    title = 'Welcome To Fro News'
+    return render_template('index.html',title = title)
+
+@app.route('/articles/<id>')
+def articles(id):
+
+    title = f'{id}'
+
+    return render_template('articles.html', title = title)
